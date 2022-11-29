01084d4fa33199fc7d7569db7ab2bfd6a2a0f8f7
adiatna sukmana <dyatna.id@gmail.com>
2022-11-29 18:53:41 +0700

    update bealright

--- lab/bealright/assets/js/app.js
+++ lab/bealright/assets/js/app.js
@@ -14,4 +14,19 @@ $(function () {
       $(".rabbit-lyrics").removeClass("hide");
     }, 2000);
   });
+
+  function openFullscreen() {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      /* Firefox */
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      /* Chrome, Safari & Opera */
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      /* IE/Edge */
+      document.documentElement.msRequestFullscreen();
+    }
+  }
 });
