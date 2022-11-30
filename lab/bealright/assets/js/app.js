08108331c25f88201a58ebb34aca17815104d325
adiatna sukmana <dyatna.id@gmail.com>
2022-11-30 20:20:12 +0700

    bealright: update visualizer

--- lab/bealright/assets/js/app.js
+++ lab/bealright/assets/js/app.js
@@ -2,16 +2,18 @@ $(function () {
   $(".loading").hide();
 
   $(".play").click(function () {
+    // openFullscreen();
     $(this).addClass("hide");
     $(".quotes").addClass("hide");
     $(".quotes-author").addClass("hide");
 
     setTimeout(() => {
-      $("#audio-1")[0].play();
+      visualizer();
     }, 1000);
 
     setTimeout(() => {
       $(".rabbit-lyrics").removeClass("hide");
+      $(".rabbit-lyrics").addClass("unhide");
     }, 2000);
   });
 
---
+++ lab/bealright/assets/js/visualizer.js
@@ -0,0 +1,54 @@
+function visualizer() {
+  var audio = document.getElementById("audio-1");
+  audio.src = "assets/bealright.mp3";
+  audio.load();
+  audio.play();
+  var context = new AudioContext();
+  var src = context.createMediaElementSource(audio);
+  var analyser = context.createAnalyser();
+
+  var canvas = document.getElementById("canvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  var ctx = canvas.getContext("2d");
+
+  src.connect(analyser);
+  analyser.connect(context.destination);
+
+  analyser.fftSize = 256;
+
+  var bufferLength = analyser.frequencyBinCount;
+  var dataArray = new Uint8Array(bufferLength);
+
+  var WIDTH = canvas.width;
+  var HEIGHT = canvas.height;
+
+  var barWidth = (WIDTH / bufferLength) * 2.5;
+  var barHeight;
+  var x = 0;
+
+  function renderFrame() {
+    requestAnimationFrame(renderFrame);
+    x = 0;
+    analyser.getByteFrequencyData(dataArray);
+
+    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    for (var i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i];
+
+      var r = barHeight + 25 * (i / bufferLength);
+      var g = 250 * (i / bufferLength);
+      var b = 50;
+
+      ctx.fillStyle = "rgba(255, 255, 255, 0.3)";
+      ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+      x += barWidth + 1;
+    }
+  }
+
+  audio.play();
+  renderFrame();
+}
